0e2a89124e83cfa263706745c2285ee4774aefb2
vaishnavi <vaishu.srini15@gmail.com>
2023-06-24 15:30:40 -0500

    Reversed number

--- practiceprobs1/src/Swap.java
+++ practiceprobs1/src/Swap.java
@@ -4,14 +4,18 @@ public class Swap {
         int b = 20;
 
         System.out.println("The values of a & b are "+ a + " & "+ b + " respectively.");
-         //let's introduce the third value t
-        int t = a;
-        a = b;
-        b = t;
+//         //let's introduce the third value t
+//        int t = a;
+//        a = b;
+//        b = t;
+//
+//        System.out.println("The values of a & b after swapping the value are "+ a + " & "+ b + " respectively.");
+
+        //logic-2 using + & _
+        a = a+b; //10+20 = 30
+        b = a-b; //30-20 = 10
+        a = a-b; //30- 10 = 20
 
         System.out.println("The values of a & b after swapping the value are "+ a + " & "+ b + " respectively.");
-
-
-
     }
 }
