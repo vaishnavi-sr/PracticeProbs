96c1b3a79ab34e6cdabe84428f3b7939e4b7fd71
vaishnavi <vaishu.srini15@gmail.com>
2023-06-24 16:02:14 -0500

    Used * & / operator to swap

--- practiceprobs1/src/Swap.java
+++ practiceprobs1/src/Swap.java
@@ -12,9 +12,14 @@ public class Swap {
 //        System.out.println("The values of a & b after swapping the value are "+ a + " & "+ b + " respectively.");
 
         //logic-2 using + & _
-        a = a+b; //10+20 = 30
-        b = a-b; //30-20 = 10
-        a = a-b; //30- 10 = 20
+//        a = a+b; //10+20 = 30
+//        b = a-b; //30-20 = 10
+//        a = a-b; //30- 10 = 20
+
+        //logic-3 using * & / operator
+        a = a*b; //10 * 20 = 200
+        b = a/b; //200/10 = 20
+        a = a/b; //200/20 = 10
 
         System.out.println("The values of a & b after swapping the value are "+ a + " & "+ b + " respectively.");
     }
