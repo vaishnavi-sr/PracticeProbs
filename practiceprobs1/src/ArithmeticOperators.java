9e9d1bd3816f9f0e426acadc9621dbb0a72fb439
vaishnavi <vaishu.srini15@gmail.com>
2024-05-23 12:24:25 -0500

    pre-increment and post-increment explained

--- practiceprobs1/src/ArithmeticOperators.java
+++ practiceprobs1/src/ArithmeticOperators.java
@@ -2,25 +2,48 @@ public class ArithmeticOperators {
 
     public static void main(String[] args) {
 
-        int num1 = 4;
+        int num = 4;
         int num2 = 6;
 
-        int addResult      = num1 + num2;
-        int subResult      = num2 - num1;
-        int multiplyResult = num1 * num2;
-        int div2Result      = num1/2;
+        int addResult      = num + num2;
+        int subResult      = num2 - num;
+        int multiplyResult = num * num2;
+        int div2Result      = num/2;
 
         System.out.println(addResult);
 //changing the value of variable since it is not declared final
-        num1 +=2; //num1= num1 + 2;
-        System.out.println(num1);//displays the new value
+        num +=2; //num= num + 2;
+        System.out.println(num);//displays the new value
         //similarly new values can be given by using arithmetic operators
 
-        num1 *= 3;
-        System.out.println(num1);
+        num *= 3;
+        System.out.println(num);
 
-        num1 /=2; //divides the new value by 2
-        System.out.println(num1);
+        num /=2; //divides the new value by 2
+        System.out.println(num);
+
+        //increment and decrement
+        num++;
+        System.out.println(num);
+
+        num--;
+        System.out.println(num);
+
+        //there can also be pre-increment and post-increment
+
+        num++; //post-increment
+        ++num; //pre-increment
+        //both are same but when we assign it to a variable it shows the difference
+        System.out.println(num);// num = 11
+
+        int result = num++;//num = 11
+        System.out.println(result);// result =11 fetch and increment
+
+        int result1 = ++num;
+        System.out.println(result1);// result1 = 13 increment and fetch
+
+
+
 
 
     }
